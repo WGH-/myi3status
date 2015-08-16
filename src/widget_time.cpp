dfba342484668d303fb1f3b21583b2446938d19b
WGH <wgh@torlan.ru>
2015-08-16 18:48:19 +0300

    added support for inotify-based /etc/localtime watch
    Immediately picks up timezone changes, without polling.

--- src/widget_time.cpp
+++ src/widget_time.cpp
@@ -4,17 +4,22 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cassert>
+#include <cstring>
 
 #include <unistd.h>
 #include <errno.h>
 #include <time.h>
+#include <limits.h>
+#include <sys/inotify.h>
 
 #include "utils.h"
 
 static const char * const FORMAT_STRING =
     "{\"full_text\": \"%F %H:%M\"}";
 
-WidgetTime::WidgetTime(EventLoop &event_loop) {
+WidgetTime::WidgetTime(EventLoop &event_loop) :
+    inotifyListener(event_loop, *this)
+{
     timerfd = create_timerfd(
             CLOCK_REALTIME,
             std::chrono::seconds(60),
@@ -23,6 +28,7 @@ WidgetTime::WidgetTime(EventLoop &event_loop) {
     );
     event_loop.add_fd(this, timerfd);
 
+    tzset();
     update_string();
 }
 
@@ -42,6 +48,11 @@ void WidgetTime::update_string() noexcept {
     assert(res != 0);
 }
 
+void WidgetTime::localtime_updated() noexcept {
+    tzset();
+    update_string();
+}
+
 const char * WidgetTime::get_string(bool force_update) noexcept {
     if (force_update) {
         update_string();
@@ -52,4 +63,56 @@ const char * WidgetTime::get_string(bool force_update) noexcept {
 void WidgetTime::descriptor_ready() noexcept {
     consume_timerfd(timerfd);
     update_string();
+}
+
+WidgetTime::InotifyListener::InotifyListener(EventLoop &event_loop, WidgetTime &widget_time)
+    : widget_time(widget_time)
+{
+    int res;
+
+    inotify_fd = inotify_init1(IN_NONBLOCK | IN_CLOEXEC);
+    assert(inotify_fd >= 0);
+
+    res = inotify_add_watch(inotify_fd, "/etc", IN_MOVED_TO | IN_CREATE);
+    assert(res >= 0);
+    etc_watch = res;
+
+    event_loop.add_fd(this, inotify_fd);
+}
+
+void WidgetTime::InotifyListener::descriptor_ready() noexcept {
+    char buf[4096]
+        __attribute__((aligned(__alignof__(struct inotify_event))));
+
+    static_assert(sizeof(buf) >= sizeof(struct inotify_event) + NAME_MAX + 1, "");
+
+    while (1) {
+        ssize_t len;
+        char *ptr;
+        const struct inotify_event *event;
+
+        len = read(inotify_fd, buf, sizeof buf);
+        if (len == -1 && errno != EAGAIN) {
+            assert(false);
+        }
+
+        if (len <= 0) break;
+
+        for (ptr = buf; ptr < buf + len; ptr += sizeof(struct inotify_event) + event->len)
+        {
+            event = (const struct inotify_event *) ptr;
+            handle_event(event);
+        }
+    }
+}
+
+void WidgetTime::InotifyListener::handle_event(const struct inotify_event *event)
+{
+    if (event->wd == etc_watch) {
+        if (event->mask & (IN_MOVED_TO | IN_CREATE)) {
+            if (strcmp(event->name, "localtime") == 0) {
+                widget_time.localtime_updated();
+            }
+        }
+    }
 }
--- src/widget_time.h
+++ src/widget_time.h
@@ -4,11 +4,29 @@
 
 class WidgetTime : public Widget, Epollable {
     int timerfd;
+
     void update_string() noexcept;
 
     char buffer[64];
+
+    class InotifyListener : public Epollable {
+        int inotify_fd; // for monitoring timezone changes
+        int etc_watch;
+        WidgetTime &widget_time;
+
+        void handle_event(const struct inotify_event *);
+
+    public:
+        InotifyListener(EventLoop &event_loop, WidgetTime &widget_time);
+        virtual void descriptor_ready() noexcept override;
+    };
+
+    InotifyListener inotifyListener;
 public:
     WidgetTime(EventLoop &event_loop);
+
+    void localtime_updated() noexcept;
+
     virtual const char* get_string(bool force_update) noexcept override;
     virtual void descriptor_ready() noexcept override;
 };
