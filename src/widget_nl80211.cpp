ba3abd31eb6d91826df14e9f12d61f215939fac7
WGH <wgh@torlan.ru>
2016-09-07 02:38:48 +0300

    hide network SSID after a while
    signal indicator can be clicked it to bring it back

--- src/widget_nl80211.cpp
+++ src/widget_nl80211.cpp
@@ -38,14 +38,24 @@ static int signal_level_from_rssi(int rssi, int levels)
     }
 }
 
-Widget_nl80211::Widget_nl80211(TimerManager &timer_manager, Nl80211 &nl80211, Rtnetlink &rtnetlink, const char *ifname, unsigned long signal_poll_interval_ms)
-    : timer_manager(timer_manager), nl80211(nl80211), rtnetlink(rtnetlink), signal_poll_interval_ms(signal_poll_interval_ms)
+Widget_nl80211::Widget_nl80211(
+        TimerManager &timer_manager,
+        OneshotTimerManager &oneshot_timer_manager,
+        ClickManager &click_manager,
+        Nl80211 &nl80211,
+        Rtnetlink &rtnetlink,
+        const char *ifname,
+        unsigned long signal_poll_interval_ms)
+    : timer_manager(timer_manager), oneshot_timer_manager(oneshot_timer_manager), nl80211(nl80211), rtnetlink(rtnetlink), signal_poll_interval_ms(signal_poll_interval_ms)
 {
     this->ifname = ifname;
 
     nl80211.add_listener(this);
     rtnetlink.add_link_listener(this);
+    click_manager.register_clickable(this);
 
+    expanded_view = true;
+    oneshot_timer_manager.schedule_after(std::chrono::seconds(1), this);
 
     update_string();
 }
@@ -59,27 +69,39 @@ void Widget_nl80211::update_string() noexcept {
 
         level = signal_level_from_rssi(info.signal_strength, sizeof(signal_strengths) / sizeof(signal_strengths[0]));
 
-        snprintf(string, sizeof(string),
-            "{\"full_text\": \"%s %s\"}",
-            signal_strengths[level],
-            info.ssid_filtered
-        );
+        if (expanded_view) {
+            snprintf(string, sizeof(string),
+                "{\"full_text\": \"%s %s\", \"name\":\"%s\", \"instance\":\"%s\"}",
+                signal_strengths[level],
+                info.ssid_filtered,
+                clickable_name(),
+                clickable_instance()
+            );
+        } else {
+            snprintf(string, sizeof(string),
+                "{\"full_text\": \"%s\", \"name\":\"%s\", \"instance\":\"%s\"}",
+                signal_strengths[level],
+                clickable_name(),
+                clickable_instance()
+            );
+        }
 
         timer_manager.register_monotonic_listener(this, std::chrono::milliseconds(signal_poll_interval_ms));
     } else {
-        const char *status = "disconnected";
-
-        if (!(link_info.iff_flags & IFF_UP)) {
-            status = "down";
+        if (link_info.iff_flags & IFF_UP) {
+            snprintf(string, sizeof(string),
+                "{"
+                    "\"full_text\": \"(%s)\","
+                    "\"color\": \"" DISCONNECTED_COLOR "\","
+                    "\"name\":\"%s\", \"instance\":\"%s\""
+                "}",
+                expanded_view ? "disconnected" : "d",
+                clickable_name(), clickable_instance()
+            );
+        } else {
+            // network is down, don't draw anything
+            strncat(string, "", sizeof(string));
         }
-
-        snprintf(string, sizeof(string),
-            "{"
-                "\"full_text\": \"(%s)\","
-                "\"color\": \"" DISCONNECTED_COLOR "\" "
-            "}",
-            status
-        );
 
         timer_manager.unregister_monotonic_listener(this);
     }
@@ -116,6 +138,8 @@ void Widget_nl80211::nl80211event(struct nl_msg *msg) noexcept {
         case NL80211_CMD_CONNECT:
         case NL80211_CMD_DISCONNECT:
         case NL80211_CMD_ROAM:
+            expanded_view = true;
+            oneshot_timer_manager.schedule_after(std::chrono::seconds(5), this);
             update_string();
             break;
         default:
@@ -138,4 +162,30 @@ void Widget_nl80211::timer_ready() noexcept {
     if (info.connected) {
         update_string();
     }
+}
+
+/*
+ * Clickable interface
+ */
+
+const char * Widget_nl80211::clickable_name() const noexcept {
+    return "nl80211";
+}
+
+const char * Widget_nl80211::clickable_instance() const noexcept {
+    return ifname;
+}
+
+void Widget_nl80211::click_handler(int button) noexcept {
+    expanded_view = true;
+    oneshot_timer_manager.schedule_after(std::chrono::seconds(3), this);
+    update_string();
+}
+
+/*
+ * One-shot timer
+ */
+void Widget_nl80211::oneshot_timer_ready() noexcept {
+    expanded_view = false;
+    update_string();
 }
--- src/widget_nl80211.h
+++ src/widget_nl80211.h
@@ -3,11 +3,13 @@
 #include <netlink/netlink.h>
 
 #include "event_loop.h"
+#include "click_manager.h"
 #include "timer.h"
+#include "timer_oneshot.h"
 #include "nl80211.h"
 #include "rtnetlink.h"
 
-class Widget_nl80211 : public Widget, Nl80211Listener, Rtnetlink::LinkListener, TimerListener {
+class Widget_nl80211 : public Widget, Nl80211Listener, Rtnetlink::LinkListener, TimerListener, OneshotTimerListener, WidgetClickable {
     char string[128];
     const char *ifname;
     unsigned long signal_poll_interval_ms;
@@ -18,13 +20,29 @@ class Widget_nl80211 : public Widget, Nl80211Listener, Rtnetlink::LinkListener, 
     Rtnetlink &rtnetlink;
     Rtnetlink::LinkInfo link_info;
     TimerManager &timer_manager;
+    OneshotTimerManager &oneshot_timer_manager;
+
+    bool expanded_view;
 
     void update_string() noexcept;
 public:
-    Widget_nl80211(TimerManager &timer_manager, Nl80211 &nl80211, Rtnetlink &rtnetlink, const char *ifname, unsigned long signal_poll_interval_ms);
+    Widget_nl80211(
+            TimerManager &timer_manager,
+            OneshotTimerManager &oneshot_timer_manager,
+            ClickManager &click_manager,
+            Nl80211 &nl80211,
+            Rtnetlink &rtnetlink,
+            const char *ifname,
+            unsigned long signal_poll_interval_ms);
 
     virtual const char* get_string(bool force_update) noexcept override;
     virtual void nl80211event(struct nl_msg *msg) noexcept override;
     virtual void link_event(struct rtnl_link *link) noexcept override;
     virtual void timer_ready() noexcept override;
+
+    virtual void click_handler(int button) noexcept override;
+    virtual const char* clickable_name() const noexcept override;
+    virtual const char* clickable_instance() const noexcept override;
+
+    virtual void oneshot_timer_ready() noexcept override;
 };
